2f49d32c68f5ab3e8977ecec5580a83dc075ab99
hiroshi kobayashi <kobayashi@ans-web.co.jp>
2015-01-09 16:31:54 +0900

    initial

--- hoge/ViewController.swift
+++ hoge/ViewController.swift
@@ -8,18 +8,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UIScrollViewDelegate {
+    @IBOutlet weak var kama: UIImageView!
+    @IBOutlet weak var board: UIScrollView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        kama.frame.size = kama.image!.size
+        board.contentSize = kama.frame.size
+        board.delegate = self
+        println(kama.frame)
+        println(kama.image?.size.width)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func viewForZoomingInScrollView(scrollView: UIScrollView) -> UIView? {
+        return kama
+    }
 
+    func scrollViewDidEndZooming(scrollView: UIScrollView, withView view: UIView!, atScale scale: CGFloat) {
+        println(scrollView.contentSize)
+        let i:AnyObject = scrollView.subviews[0]
+        if i is UIImageView{
+            let size = (i as UIImageView).image!.size
+            println(size)
+            scrollView.contentSize = CGSizeMake(size.width * scale, size.height  * scale);
+        }
+    }
 
 }
 
