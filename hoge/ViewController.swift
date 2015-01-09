c5cdc049b52442cc16bb651fdd5937a1affe448f
hiroshi kobayashi <kobayashi@ans-web.co.jp>
2015-01-09 16:51:58 +0900

    add comments

--- hoge/ViewController.swift
+++ hoge/ViewController.swift
@@ -14,12 +14,10 @@ class ViewController: UIViewController,UIScrollViewDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        kama.frame.size = kama.image!.size
-        board.contentSize = kama.frame.size
-        board.delegate = self
-        println(kama.frame)
-        println(kama.image?.size.width)
+        
+        kama.frame.size = kama.image!.size // ImageView に画像のサイズを設定。画像を動的に取得するときのために。
+        board.contentSize = kama.frame.size // ScrollView に画像のサイズを設定
+        board.delegate = self // board のメソッド実装先を自分にする
     }
 
     override func didReceiveMemoryWarning() {
@@ -27,16 +25,21 @@ class ViewController: UIViewController,UIScrollViewDelegate {
         // Dispose of any resources that can be recreated.
     }
     
+    
+    // MARK: UIScrollViewDelegate
     func viewForZoomingInScrollView(scrollView: UIScrollView) -> UIView? {
         return kama
     }
 
     func scrollViewDidEndZooming(scrollView: UIScrollView, withView view: UIView!, atScale scale: CGFloat) {
-        println(scrollView.contentSize)
+        // ズームにより scrollView のサイズが変更されてしまうので、
+        // ズーム終了時に再設定する。
         let i:AnyObject = scrollView.subviews[0]
-        if i is UIImageView{
+        if i is UIImageView{ // 画像ならば
+            // UIView から UIImageView にダウンキャストする
+            // UIView -> UIImageView ダウンキャスト
+            // UIImageView -> UIView アップキャスト
             let size = (i as UIImageView).image!.size
-            println(size)
             scrollView.contentSize = CGSizeMake(size.width * scale, size.height  * scale);
         }
     }
